9ab65c99331396238b1e36ebd1b1cc73f80844a2
Zachary Mays <zackmays@gmail.com>
2016-05-24 18:36:23 -0700

    Adds basic user tests

--- tests/integration/models/UserTest.php
+++ tests/integration/models/UserTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\User;
+use App\Project;
+
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -16,5 +18,28 @@ class UserTest extends TestCase
 
     	$users = User::all();
         $this->assertEquals(3, $users->count());
+    }
+
+    /** @test */
+    public function a_user_has_all_attributes()
+    {
+    	$user = factory(User::class)->create();
+
+    	$this->assertNotEmpty($user->first_name);
+    	$this->assertNotEmpty($user->last_name);
+    	$this->assertNotEmpty($user->email);
+    	$this->assertNotEmpty($user->password);
+    }
+
+    /** @test */
+    public function a_user_has_projects()
+    {
+    	$user = factory(User::class)->create();
+    	$project1 = factory(Project::class)->create(['user_id' => $user->id]);
+    	$project2 = factory(Project::class)->create(['user_id' => $user->id]);
+
+    	$this->assertEquals($user->id, $project1->user->id);
+    	$this->assertEquals($user->id, $project2->user->id);
+    	$this->assertEquals(count($user->projects), 2);
     }
 }
