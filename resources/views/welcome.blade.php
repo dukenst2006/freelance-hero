6779761d96144bbb6328f88f02ec3e29d22ae767
Zachary Mays <zackmays@gmail.com>
2016-06-03 17:46:48 -0700

    Remakes 'end session' form with form helper to prevent missing token error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,12 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <p>Your Application's Landing Page.</p>
+                    <p>&nbsp;</p>
                     @if ( Session::has('active_work_session') )
-                    <form method="POST" action="{{ url('/work_sessions/end') }}">
-                    <button type="submit">End Session</button>
-                    </form>
+                        {!! Form::open(array('action' => 'WorkSessionsController@end', 'class' => 'form-horizontal', 'role' => 'form')) !!}
+                            {!! Form::submit('End Session', ['class' => 'btn btn-primary']) !!}
+                        {!! Form::close() !!}
                     @endif
                 </div>
             </div>
