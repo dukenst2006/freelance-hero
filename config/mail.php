ec3790a5feb3354485ec73f8afd0b7723192446e
Zachary Mays <zackmays@gmail.com>
2016-07-18 14:02:33 -0700

    Updates from email address and name

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'info@freelance-hero.com', 'name' => 'FreeLance Hero'],
 
     /*
     |--------------------------------------------------------------------------
