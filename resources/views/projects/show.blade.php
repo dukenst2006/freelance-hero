36209a78c990d30ad403014820d314beec58aa87
Zachary Mays <zackmays@gmail.com>
2016-10-12 14:22:18 -0700

    Displays full list of work sessions on projects page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -26,5 +26,23 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Work Sessions</div>
+
+                <div class="panel-body">
+                    @if ( count($project->work_sessions) > 0 )
+                        @foreach ( $project->work_sessions as $work_session )
+                            <p>{{ date_format( date_create($work_session->end_time), 'l, m/d/Y') }}, {{ $work_session->project->name }}: <strong>{{ $work_session->total_hours }}hr(s)</strong></p>
+                        @endforeach
+                    @else
+                        <p>No completed work sessions.</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
